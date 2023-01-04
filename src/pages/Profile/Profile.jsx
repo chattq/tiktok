407c943bb7ae8825886693c9ae4d971cc3c42450
chattq <baotuyet927@gmail.com>
2023-01-04 19:50:23 +0700

    quang setup du án

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      Profile
+      <a href=''>chúng ta không thuốc</a>
+      <Controller
+        control={control}
+        name='dateOfBirth'
+        render={({ field }) => (
+          <DateSelect errorMessage={errors.dateOfBirth?.message} onChange={field.onChange} value={field.value} />
+        )}
+      ></Controller>
+    </div>
   )
 }
