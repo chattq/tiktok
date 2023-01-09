10b229356966b1d11386ad43255fce94bcc7614d
chattq <baotuyet927@gmail.com>
2023-01-09 21:50:32 +0700

    quang update

--- src/pages/Home/Views/Sidebar/SideBar.jsx
+++ src/pages/Home/Views/Sidebar/SideBar.jsx
@@ -16,7 +16,6 @@ export default function SideBar() {
   useEffect(() => {
     const getAcounts = async () => {
       const result = await User.suggestUserList({ page, perPage })
-      console.log(result.data.data)
       setAllSuggestedUsers(result.data.data)
       const lessResult = result.data.data.slice(0, 5)
       setSuggestedUsers(lessResult)
@@ -30,7 +29,6 @@ export default function SideBar() {
     seeMore ? setData(suggestedUsers) : setData(allSuggestedUsers)
     setSeeMore(!seeMore)
   }
-  console.log(data)
   return (
     <>
       <div className='h-[156px] border-b border-[#F1F1F2]'>
@@ -48,9 +46,9 @@ export default function SideBar() {
               </Link>
             ))}
         </div>
-        <h4 onClick={handleSeeAll} className='mt-4 text-fontSizeTitle font-semibold text-tiktokPink'>
+        <span onClick={handleSeeAll} className='mt-4 cursor-pointer text-fontSizeTitle font-semibold text-tiktokPink'>
           {seeMore ? `Ẩn bớt` : `Xem tất cả`}
-        </h4>
+        </span>
       </div>
       <div className='mt-3 border-t border-[#F1F1F2] px-2 py-2'>
         <h4 className='text-fontSizeTitle font-semibold text-tiktokColorText'>Khám phá</h4>
